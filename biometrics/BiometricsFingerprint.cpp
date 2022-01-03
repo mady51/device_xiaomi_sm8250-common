b70a3833fac0ff91454995eb22d56775f813ff87
EcrosoftXiao <xy19990903@gmail.com>
2022-01-03 22:41:12 +0000

    sm8250-common: fingerprint: Call OnFingerUp while postenroll and authenticated

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -238,6 +238,7 @@ Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69
 }
 
 Return<RequestStatus> BiometricsFingerprint::postEnroll() {
+    onFingerUp();
     return ErrorFilter(mDevice->post_enroll(mDevice));
 }
 
@@ -404,6 +405,7 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
                         token).isOk()) {
                     ALOGE("failed to invoke fingerprint onAuthenticated callback");
                 }
+                getInstance()->onFingerUp();
             } else {
                 // Not a recognized fingerprint
                 if (!thisPtr->mClientCallback->onAuthenticated(devId,
